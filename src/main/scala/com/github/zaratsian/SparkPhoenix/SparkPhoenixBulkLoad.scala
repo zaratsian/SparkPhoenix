482b31085cfaaf9d8c26a7187c1066502517a780
Dan Zaratsian <dtz001@gmail.com>
2016-10-22 14:51:06 -0400

    Update SparkPhoenixBulkLoad.scala

--- src/main/scala/com/github/zaratsian/SparkPhoenix/SparkPhoenixBulkLoad.scala
+++ src/main/scala/com/github/zaratsian/SparkPhoenix/SparkPhoenixBulkLoad.scala
@@ -7,7 +7,7 @@ This code does the following:
   3) Save the RDD to Phoenix formatted HFiles.
 
 Usage:
-spark-submit --class com.github.zaratsian.SparkPhoenix.SparkPhoenixBulkLoad --jars /tmp/SparkHBaseExample-0.0.1-SNAPSHOT.jar /usr/hdp/current/phoenix-client/phoenix-client.jar /tmp/props
+spark-submit --class com.github.zaratsian.SparkPhoenix.SparkPhoenixBulkLoad --jars /tmp/SparkPhoenix-0.0.1.jar /usr/hdp/current/phoenix-client/phoenix-client.jar /tmp/props
 
 ********************************************************************************************************/  
 
@@ -20,6 +20,7 @@ import org.apache.spark.sql.functions.avg
 import scala.collection.mutable.HashMap
 import scala.io.Source.fromFile
 import scala.collection.JavaConverters._
+import scala.reflect.ClassTag
 
 import org.apache.hadoop.hbase.client.Scan
 import org.apache.hadoop.hbase.protobuf.ProtobufUtil
@@ -39,6 +40,7 @@ import org.apache.hadoop.hbase.KeyValue
 import org.apache.hadoop.hbase.mapred.TableOutputFormat
 import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat
 import org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles
+import org.apache.hadoop.hbase.mapreduce.{HFileOutputFormat2, TableMapReduceUtil}
 
 import org.apache.hadoop.mapred.JobConf
 import org.apache.hadoop.mapreduce.Job
@@ -63,32 +65,45 @@ object SparkPhoenixBulkLoad{
         
         val props = getProps(args(0))
         
-        val sparkConf = new SparkConf().setAppName("SparkHBaseBulkLoad")
+        val sparkConf = new SparkConf().setAppName("SparkPhoenix_Create_HFiles")
         val sc = new SparkContext(sparkConf)
         
         val sqlContext = new org.apache.spark.sql.SQLContext(sc)
         import sqlContext.implicits._
         
         // Configure HBase output settings
+        println("[ *** ] Configuring HBase")
         val htablename     = "sparkphoenixtable"
         val hfile_location = "/tmp/sparkphoenixtable"
         val hConf          = HBaseConfiguration.create()
             hConf.set("zookeeper.znode.parent", "/hbase-unsecure")
         
-        val job: Job = Job.getInstance(hConf, "Phoenix bulk load")
+        println("[ *** ] Get Job Instance")
+        val job: Job = Job.getInstance(hConf, "Phoenix bulkload")
             job.setMapOutputKeyClass(classOf[ImmutableBytesWritable])
             job.setMapOutputValueClass(classOf[KeyValue])
         
         TableMapReduceUtil.initCredentials(job)
         
+        println("[ *** ] Creating HTable")
         val htable: HTable = new HTable(hConf, htablename)
         
+        println("[ *** ] Configuring Incremental Load")
         HFileOutputFormat2.configureIncrementalLoad(job, htable)
         
         // Create test RDD (1 million records)
+        println("[ *** ] Creating arbitrary RDD with 1 million records and 3 fields [integer, string, float]")
         val range = 1 to 1000000
-        val rdd   = sc.parallelize(range).map(x => List( x, "arbitrary_string_"+x.toString(), (x.toFloat()/1000000) ))
-        
+
+        val rdd_from_df = sc.parallelize(range).map(x => {
+            val kv: KeyValue = new KeyValue( Bytes.toBytes(x(0).asInstanceOf[Int]), x(1).toString.getBytes(), x(2).toString.getBytes(), x(3).asInstanceOf[Long], x(6).toString.getBytes() )
+            (new ImmutableBytesWritable( Bytes.toBytes(x(0).asInstanceOf[Int]) ), kv)
+        })
+
+
+        println("[ *** ] Printing first 5 records of RDD")
+        rdd.take(5).foreach(x=>println(x))
+
         //rdd.mapPartitions(PartitionSorter.sortPartition).saveAsNewAPIHadoopFile(
         rdd.saveAsNewAPIHadoopFile(
             hfile_location,
